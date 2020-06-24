873b996bf3b2759c410ce01956506ec798e77838
wangwanbing <67352899+wwbhwwb@users.noreply.github.com>
2020-06-24 16:05:24 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from gamekj import *
+
+def main():
+    game = Game(
+        r"C:\Users\Administrator\Desktop\Python3X\上课\上传处\game\飞机大战\image\icon.png", "wwb的游戏", 480, 680)
+    game.run()
+
+if __name__ == "__main__":
+    main()
